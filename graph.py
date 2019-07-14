40cdf4bc5ecc04c0b659b23a9dd825ed3b5a4f19
Thinker222 <es0197@yahoo.com>
2019-07-14 15:53:02 -0400

    Start of djikstra

--- graph.py
+++ graph.py
@@ -1,3 +1,5 @@
+import math
+import numpy as np
 
 # Graph represent a series of nodes as a graph. 
 # The weights are represented by a dictionary. Each entry in the dictionary will be a node.
@@ -5,31 +7,44 @@
 
 class Graph: 
     def __init__(self):
-        self.__weights = {}
+        self.__nodes = {}
     
     # Get a djiskstra table so actors can make decisions
-    def getDjikstraTable(self):
+    def getDjikstraTable(self, id):
+        node = self.__nodes[id]
+        dim = len(list(self.__nodes.keys())) - 1
+        table = np.zeros(shape=(dim, dim))
+        table = np.subtract(table, 1)
+        unFinishedIds = list(self.__nodes.keys())
+        finishedIds = {}
+        while len(unFinishedIds) != 0:
+            bestWeight = unFinishedIds[0]
+
+
+
+
         return None
 
     # Set a weight between two nodes. 
     # First you delete any existing weights between the two nodes. 
     # Then you set a weight going from one id to the other and vice versa. 
     # Remember, the graph is not directed
-    def setWeight(self, id1, id2):
-        return None 
+    def setWeight(self, id, id2, val, estimate=True):
+        self.__nodes[id].weights[id2] = val
+        self.__nodes[id2].weights[id] = val
     
     # Add a node to the weights dictionary
     def addNode(self, node):
-        return None
+        self.__nodes[node.id] = node
 
     # Remove a node from the weights dictionary, 
     # and remove all entries of that node in the other tables
     def removeNode(self, id):
-        return None
+        self.__nodes[id] = None
 
     # Get
     def getNode(self, id):
-        return None 
+        return self.__nodes[id]
 
 
 # Node represents a single nod in a graph
@@ -38,10 +53,12 @@ class Node:
     def __init__(self, x = 0, y = 0):
         self.id = Node.currentId
         self.coords = (x, y)
-        self.weights = []
+        # Key is a node id
+        # Value is a weight
+        self.weights = {}
         Node.currentId = Node.currentId + 1
 
     # Estimates the weight from another node 
     def estimateWeight(self, node):
-        return None
+        return math.sqrt(math.pow(self.coords[0] - node.coords[0], 2) + math.pow(self.coords[1] - node.coords[1], 2))
 
